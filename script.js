57dbf66daa097fd9c119551191f9f5b425467590
chamudith bandara <chamudithbuddima@gmail.com>
2026-04-03 21:24:53 +0530

    refactor: update input handling in calculate function and enhance HTML structure

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function calculate() {
-    let input1 = 0.5;
-    let input2 = 0.8;
-    
+    let input1 = document.getElementById("input1").value;
+    let input2 = document.getElementById("input2").value;
+
     let Weight1 = 0.6;
     let Weight2 = 0.2;
     let bias = -2;
 
     let weightedSum = (input1 * Weight1) + (input2 * Weight2) + bias;
-    console.log(weightedSum);
+    // console.log(weightedSum);
+
+    let activationOutput = 1 / (1 + Math.exp(-weightedSum));
+    console.log(activationOutput);
 
 }
