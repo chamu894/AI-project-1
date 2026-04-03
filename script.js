73744f48ce7644a40eca8ddfa49a9fd72832b3f2
chamudith bandara <chamudithbuddima@gmail.com>
2026-04-03 21:08:55 +0530

    add index.html and script.js with initial implementation

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculate() {
+    let input1 = 0.5;
+    let input2 = 0.8;
+    
+    let Weight1 = 0.6;
+    let Weight2 = 0.2;
+    let bias = -2;
+
+    let weightedSum = (input1 * Weight1) + (input2 * Weight2) + bias;
+    console.log(weightedSum);
+
+}
